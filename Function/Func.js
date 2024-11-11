df8f790a887e70be8e2b3a4b9a983af636087b69
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2024-11-11 22:52:52 +0530

    Add files via upload
    starting function

---
+++ Function/Func.js
@@ -0,0 +1,11 @@
+var d=add(8,8);
+console.log(d);//Hosting in function
+let e=add(4,4);
+console.log(e);//Hosting the funtion
+function add(a,b){
+    var c= a+b;
+    return c;
+}
+let totla=add(90,90);
+console.log(totla);
+
